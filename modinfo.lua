3bc1ad679c20cc145b2cbcbe5722c0dd84ceb05a
pandinh2007 <57869841+pandinh2007@users.noreply.github.com>
2024-07-17 13:33:01 +0700

    4.0.22.5

--- modinfo.lua
+++ modinfo.lua
@@ -7,7 +7,7 @@ Thank you for using this mod, you can use the config button below and let me kno
 	
 ]]
 author = "Mio, dinhkarate, Haruhi Kawaii"
-version = "4.1.21.1"
+version = "4.0.22.5"
 forumthread = ""
 
 folder_name = folder_name or "workshop-"
