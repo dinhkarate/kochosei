4bf1097f0b387d5474f59ceb55707e23fe215102
haruhikawaii2k1 <57211540+haruhikawaii2k1@users.noreply.github.com>
2024-07-23 22:17:28 +0700

    Update modclientmain.lua

--- modclientmain.lua
+++ modclientmain.lua
@@ -1,5 +1,4 @@
 PrefabFiles = {
-
 	"kochosei",
 	"kochosei_none",
 }
@@ -11,19 +10,6 @@ GLOBAL.setmetatable(env, {
 GLOBAL.Kochoseiapi = env
 modimport("scripts/kochoas")
 
-local skin_modes = {
-	{
-		type = "ghost_skin",
-		anim_bank = "ghost",
-		idle_anim = "idle",
-		scale = 0.75,
-		offset = {
-			0,
-			-25,
-		},
-	},
-}
-
-AddModCharacter("kochosei", "FEMALE", skin_modes)
+AddModCharacter("kochosei", "FEMALE")
 
 modimport("scripts/api_skins_soraaaaa") -- Không thấy modded nên dùng api đáng lẽ api cũ cơ
