2f456cd28b053a82dded8308a24559063e0df51e
Pham Quang Dinh <57869841+dinhkarate@users.noreply.github.com>
2024-09-14 14:55:47 +0700

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -9,7 +9,7 @@ Thank you for using this mod, you can use the config button below and let me kno
 author = "Mio, dinhkarate, Haruhi Kawaii"
 
 
-version = "4.2.0"
+version = "4.2.4"
 
 
 forumthread = ""
